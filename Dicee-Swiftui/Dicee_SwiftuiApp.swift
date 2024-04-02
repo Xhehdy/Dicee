685bc0c02bc5dd8740b5a2784d304a87a3d88788
Ikechukwu Zion <ikechukwuzion@gmail.com>
2024-04-02 17:42:13 +0100

    Initial Commit

---
+++ Dicee-Swiftui/Dicee_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftuiApp.swift
+//  Dicee-Swiftui
+//
+//  Created by Ikechukwu Zion on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
